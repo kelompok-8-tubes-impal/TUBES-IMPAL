47ae945ea3f6944d9db9ea6e32af663990998dbc
Rolanda <rolandadifandana@gmail.com>
2019-01-29 22:22:14 +0700

    insert record to database

---
+++ databaseRentalMobil.sql
@@ -0,0 +1,35 @@
+insert into Customer values
+('13009876155532','Ahmad Whafa','082118364897','jl.pisangan no 21')
+('13004456356242','Rizaldi Hartono','081277853524','jl.buahbatu no 11')
+('13002311238884','Sinta lusianti','082114654378','jl.kopoindah no 86')
+('13003277856361','Abdulrizal','081223165786','jl.bojongsoang no16')
+
+insert into Rental_mobil values
+('44389','Modis Rental','02114358','jl.telekomunikasi no 25')
+('44125','Makmur Rental','082767843678','jl.mutikasari no 44')
+('44779','Jaya Rental','082887573645','jl.gajahmada no 51')
+('44221','mawar Rental','081998732768','jl.indahsari no 35')
+
+insert into Mobil values
+('B3284RT','44389','Toyota','Avanza','6','2010',250000)
+('D1421ZX','44125','Toyota','Xenia','6','2012',30000)
+('D2289XCV','44779','Honda','Jazz','5','2010',300000)
+('D9901X','44221','Toyota','Alphard','6','2014',350000)
+
+insert into transaksi values
+('12677','13009876155532','B3284RT','2019-01-31',3,900000)
+('11643','13004456356242','D1421ZX','2019-01-02',1,250000)
+('13227','13002311238884','D2289XCV','2019-01-12',7,1300000)
+('14332','13003277856361','D9901X','2018-12-21',30,4500000)
+
+insert into Rekap_pembayaran values
+('0112','2019-02-01')
+('1453','2019-01-03')
+('2212','2019-01-14')
+('5332','2018-12-22')
+
+insert into Pembayaran values
+('10202123','12677','0112','Bca','2019-01-29')
+('12321992','11643','1453','Mandiri','2019-01-02')
+('19983721','13227','2212','Bca','2019-01-11')
+('15546312','14332','5332','BNI','2018-12-20')
